1bda7dc87b36d27f21e6c3d0b3b8f4f0bfbbf49f
arima0714 <a1710021@edu.cc.uec.ac.jp>
2020-06-02 23:20:25 +0900

    gensimを経由してW2Vを導入した

--- volume/index.py
+++ volume/index.py
@@ -1,8 +1,15 @@
 from flask import *
 
+import gensim
+model = gensim.models.Word2Vec.load("model_dev.model")
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return jsonify({"language":"python"})
+
+@app.route("/test")
+def testx():
+    return make_response(jsonify({"ruigigo":"ruigigo"}))
 
