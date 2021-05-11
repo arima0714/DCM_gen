b476064419ad64a23f184fa18cab829f03a95e23
arima0714 <32933855+arima0714@users.noreply.github.com>
2021-05-11 10:36:03 +0900

    単語を入力するとモデルから入力された単語の類似度が高い単語を10個表示するようにした

--- volume/index.py
+++ volume/index.py
@@ -1,9 +1,20 @@
-import streamlit
+import streamlit as st
 
-import gensim
+import gensim.models as word2vec
+import pandas as pd
 
-model = gensim.models.Word2Vec.load("dosukebe.model")
+model = word2vec.Word2Vec.load("dosukebe.model")
 
 """
 # 類語探索
 """
+
+user_input = ""
+user_input = st.text_input("類義語のリストが欲しい単語を入力してください")
+if user_input != "":
+    similar = model.wv.most_similar(user_input)
+    columns = ["類似する単語", "類似度"]
+    DF = pd.DataFrame(similar)
+    DF.columns = columns
+    DF.set_index(columns[0])
+    st.dataframe(DF)
